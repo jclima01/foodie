7086f69ef4ad5521f7f96c1c6003b879885ce8ba
Juan Cruz Lima Caramico <juan.mdq22@gmail.com>
2023-04-29 14:24:03 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -5,6 +5,7 @@ const path = require("path");
 const { DATABASE_URL } = process.env;
 const DietModel = require("./models/Diet.js");
 const RecipeModel = require("./models/Recipe.js");
+
 const sequelize = new Sequelize(`${DATABASE_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
